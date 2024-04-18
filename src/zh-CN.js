2455f52450226750fa1948c117ac8eb79f2740a2
Wilson40172 <kelvin811215@hotmail.com>
2024-04-18 15:02:04 +0800

    first commit 2

---
+++ src/zh-CN.js
@@ -0,0 +1,15 @@
+// zh-CN.js
+import { Locale } from 'date-fns';
+
+const zhCN = {
+  code: 'zh-CN',
+  formatDistance: () => '',
+  formatLong: {},
+  formatRelative: () => '',
+  localize: {},
+  match: {},
+  options: {},
+  timeFormat: {},
+};
+
+export default zhCN;
